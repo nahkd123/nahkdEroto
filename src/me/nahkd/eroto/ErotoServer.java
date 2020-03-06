d68d1a7893fd201913c34192a4b59df3dee3e909
nahkd123 <nahkd123@gmail.com>
2020-03-06 10:53:26 +0700

    add the shutdown function

--- src/me/nahkd/eroto/ErotoServer.java
+++ src/me/nahkd/eroto/ErotoServer.java
@@ -3,14 +3,19 @@ package me.nahkd.eroto;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public abstract class ErotoServer extends Thread {
 	
-	ServerSocket server;
+	public ServerSocket server;
+	boolean shutdownRequest;
+	int port;
 	
 	public ErotoServer(int port) {
 		try {
+			this.port = port;
 			server = new ServerSocket(port);
+			shutdownRequest = false;
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -21,13 +26,28 @@ public abstract class ErotoServer extends Thread {
 	@Override
 	public void run() {
 		try {
-			while (isAlive()) {
+			while (!shutdownRequest) {
 				Socket sock = server.accept();
+				if (shutdownRequest) break;
 				System.out.println("Eroto: New connection!");
 				ErotoConnection con = createConnection(sock);
 				con.setup(ErotoConnection.MESSAGETYPE_STRING);
 				con.start();
 			}
+			System.out.println("Eroto Server: Closed");
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void shutdown() {
+		shutdownRequest = true;
+		try {
+			new Socket("localhost", port).close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			// Wait... localhost is invaild?
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
